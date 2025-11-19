3c7c9b1411ee3cce7398fe7a1e09ee3761f6c719
ezequiel-nahuel-fernandez-zalazar <ezequielgamer60@gmail.com>
2025-11-18 21:56:39 -0300

    Subida atrasada del TP4

---
+++ tp4Socket_Mini-chat/src/main/java/tp4/mainClient.java
@@ -0,0 +1,7 @@
+package tp4;
+
+public class mainClient {
+    public static void main(String[] args) {
+        new cliente();
+    }
+}
---
+++ tp4Socket_Mini-chat/src/main/java/tp4/mainServidor.java
@@ -0,0 +1,7 @@
+package tp4;
+
+public class mainServidor {
+    public static void main(String[] args) {
+        new servidor();
+    }
+}
